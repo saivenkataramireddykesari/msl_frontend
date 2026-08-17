d90f6dbb2c12a45c659ae6d346a2c4839898becb
Sai Venkata Rami Reddy Kesari <ramireddy@pulsepharma.in>
2026-08-17 12:08:54 +0530

    changes done on aug 17th

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "https://msl-backend-1.onrender.com//api";
+const API_URL = (import.meta.env.VITE_API_URL || "https://msl-backend-1.onrender.com/api").replace(/\/+$/, "");
 
 // 
 const api = axios.create({
